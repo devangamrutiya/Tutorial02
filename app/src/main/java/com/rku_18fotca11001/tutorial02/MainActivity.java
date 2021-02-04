94f6f0e5f2bc12e907fbb05f068cef5033f94ac0
Devang <damrutiya706@rku.ac.in>
2021-02-04 19:11:53 +0530

    Tutorial 02

---
+++ app/src/main/java/com/rku_18fotca11001/tutorial02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rku_18fotca11001.tutorial02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
